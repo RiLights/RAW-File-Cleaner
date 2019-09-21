7e8ec0ab9602549778544e62609026ec651ad4f7
Ostap <rilights@gmail.com>
2019-09-21 16:58:56 +1200

    cleaning

--- RAW File Cleaner/AppDelegate.swift
+++ RAW File Cleaner/AppDelegate.swift
@@ -15,47 +15,22 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     var window: NSWindow!
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        let visualEffect = NSVisualEffectView()
-        visualEffect.blendingMode = .behindWindow
-        visualEffect.state = .active
-        visualEffect.material = .light
         
-        //print("1debug")
-        // Create the SwiftUI view that provides the window contents.
-        //var contentView = ContentView()
         let content = NSHostingController(rootView: MainView())
         
         // Create the window and set the content view.
         window = NSWindow(contentViewController: content)
-//        window = NSWindow(
-//            contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
-//            styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
-//            backing: .buffered, defer: false)
         window.center()
         window.title = ""
         window.styleMask.remove(.resizable)
-        //window.appearance = NSVisualEffectView()//NSAppearance(named: .aqua)
-        //window.setFrameAutosaveName("Main Window")
-        //window.contentView = NSHostingView(rootView: contentView)
-
-        //window?.contentView = visualEffect
-        //var hh = NSHostingView(rootView: contentView)
-        //hh.allowsVibrancy = true
-        //window.appearance = NSAppearance(named: .vibrantLight)
-        //window.contentView = NSHostingController(rootView: ContentView())
-        //window.contentView = visualEffect
-        //window.backgroundColor = .systemBlue
-        //window.appearance = NSAppearance(named: .aqua)
-        //window.contentView?.addSubview(visualEffect)
+        
         window.makeKeyAndOrderFront(nil)
         window.titlebarAppearsTransparent = true
-        //window.handleClose(<#T##command: NSCloseCommand##NSCloseCommand#>)
-        //NSCloseCommand(test)
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
-        print("terminate debug")
+        //print("terminate debug")
         //NSApplication.shared.terminate(self)
     }
 
@@ -64,6 +39,5 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         return true
     }
     
-
 }
 
